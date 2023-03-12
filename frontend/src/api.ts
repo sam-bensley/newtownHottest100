05521b4845afd0ebcb03dd43b66ac50ab8ca7679
sam-bensley <sam.bensley@me.com>
2023-03-12 21:51:50 +1100

    api end point env

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,7 +1,8 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://localhost:8080/'; // Local
+// axios.defaults.baseURL = 'http://localhost:8080/'; // Local
+axios.defaults.baseURL = process.env.REACT_APP_API_URL; // Production
 
 export const performGetRequest = async (url: string, params: any = {}) => {
   return axios.get(url, {
